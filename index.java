f94df83aa1672de7d7e0fb01ecf81a51c6750713
Joy Ohue <joyohue@gmail.com>
2019-08-29 07:23:55 +0100

    userinput

--- index.java
+++ index.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Joy{
   public static void main (String[]args){
     Scanner joy =new Scanner (System.in);
+    System.out.print("How old Are You?...\n");
+    int age = joy.nextInt();
     System.out.println("Hello Vicky")
     
   }
